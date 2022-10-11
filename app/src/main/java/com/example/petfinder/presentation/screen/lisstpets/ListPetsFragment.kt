bbab8ba5b816bee0643b030c6a31a8c864c4cb10
GrigoryKharchenko <96061286+GrigoryKharchenko@users.noreply.github.com>
2022-10-11 19:07:15 +0600

    #: Add dataStore and Paging.

--- app/src/main/java/com/example/petfinder/presentation/screen/lisstpets/ListPetsFragment.kt
+++ app/src/main/java/com/example/petfinder/presentation/screen/lisstpets/ListPetsFragment.kt
@@ -1,16 +1,21 @@
-package com.example.petfinder.presentation.ui.lisstpets
+package com.example.petfinder.presentation.screen.lisstpets
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import com.example.petfinder.R
 import com.example.petfinder.databinding.FragmentListPetsBinding
+import com.example.petfinder.extensions.launchWhenStarted
 import com.example.petfinder.presentation.base.BaseFragment
-import com.example.petfinder.presentation.ui.lisstpets.adapter.ListPetsAdapter
-import com.example.petfinder.presentation.ui.model.PetModel
-import com.example.petfinder.presentation.ui.profilepet.ProfilePetFragment
+import com.example.petfinder.presentation.screen.lisstpets.adapter.ListPetsAdapter
+import com.example.petfinder.presentation.screen.model.PetModel
+import com.example.petfinder.presentation.screen.profilepet.ProfilePetFragment
+import kotlinx.coroutines.flow.onEach
 
 class ListPetsFragment : BaseFragment<ListPetsViewModel>() {
 
@@ -33,16 +38,20 @@ class ListPetsFragment : BaseFragment<ListPetsViewModel>() {
         super.onViewCreated(view, savedInstanceState)
         with(binding) {
             rvListPets.adapter = adapter
-            //TODO fix subscribe
-            lifecycleScope.launchWhenStarted {
-                viewModel.petFlow.collect(adapter::submitList)
+            etSearchPet.doOnTextChanged { text, _, _, _ ->
             }
-            tilSearchPet.setEndIconOnClickListener {
-                viewModel.findPets(etSearchPet.text.toString())
-            }
-            swipeRefresh.setOnRefreshListener {
-                viewModel.getListPets()
-            }
+        }
+        with(viewModel) {
+            pets.onEach { petModels ->
+                adapter.submitData(petModels)
+                binding.swipeRefresh.isRefreshing = false
+            }.launchWhenStarted(lifecycleScope, viewLifecycleOwner.lifecycle)
+        }
+        binding.swipeRefresh.setOnRefreshListener {
+            adapter.refresh()
+        }
+        adapter.addLoadStateListener { state ->
+            binding.flProgress.isVisible = state.refresh == LoadState.Loading
         }
     }
 
